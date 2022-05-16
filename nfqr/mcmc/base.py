fixed ess calculation

nfqr/mcmc/base.py
@@ -1,7 +1,10 @@
 import numpy as np
 import torch
 import unew.analysis as un
+from unew.errors import NoFluctuationsError
+from nfqr.utils.misc import create_logger
 
+logger = create_logger(__name__)
 
 class MCMC(object):
     def __init__(self, n_steps) -> None:
@@ -50,10 +53,21 @@ def basic_integrated_ac(history):
     history_c = history - history.mean()
     ac = np.correlate(history_c, history_c, mode="full")
     ac = ac[N - 1 :] / np.arange(N, 0, -1)
-    return ac / ac[0]
+    normed_ac = ac / ac[0]
+
+    integration_length = int(.1 * N)
+    tau_int = normed_ac[:integration_length].sum()
+
+    return tau_int
 
 
-def uw_analysis(history):
+def uw_analysis(history,max_rep_size=1e6):
+
+    # floored
+    # rep_size = int(min(max_rep_size,len(history)))
+
+    # # max not necessary, but makes it manifest
+    # num_rep = max(int(len(history)/rep_size),1)
 
     if isinstance(history, torch.Tensor):
         history = history.numpy()
@@ -69,12 +83,15 @@ def uw_analysis(history):
         tau = results.tau_int[0]
         dtau = results.dtau_int[0]
 
-    except un.errors.NoFluctuationsError:
+    except NoFluctuationsError:
         mean, error, tau, dtau = 0, 0, 0, 0
 
     return mean, error, tau, dtau
 
 
 def get_mcmc_statistics(history):
-    mean, error, tau, dtau = uw_analysis(history)
-    return {"mean": mean, "error": error, "tau_int": tau, "dtau_int": dtau}
+
+    mean, error_unew, tau_unew, dtau_unew = uw_analysis(history)
+    tau_int_basic = basic_integrated_ac(history)
+
+    return {"mean": mean, "error": error_unew, "tau_int": tau_unew, "dtau_int": dtau_unew,"tau_int_basic":tau_int_basic}