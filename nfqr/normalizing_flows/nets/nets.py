10e1103f5f67027cbc212112d11760555de1f62b
Fabian <fabian.dechent@campus.tu-berlin.de>
2022-08-09 13:52:49 +0000

    net error caught

--- nfqr/normalizing_flows/nets/nets.py
+++ nfqr/normalizing_flows/nets/nets.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 from typing import List, Literal, Optional, Union
 
 import torch
@@ -270,17 +271,21 @@ class AtrousConvolution(nn.Module):
         self.convs = nn.ModuleList()
 
         for d in dilations:
-            self.convs.append(
-                nn.Conv1d(
-                        in_channels=in_channels,
-                        out_channels=out_channels,
-                        kernel_size=kernel_size,
-                        stride=stride,
-                        padding=((kernel_size-1)*d)//2,
-                        padding_mode=padding_mode,
-                        groups=groups,
-                        dilation=d
-                    ))
+            try:
+                cnn = nn.Conv1d(
+                            in_channels=in_channels,
+                            out_channels=out_channels,
+                            kernel_size=kernel_size,
+                            stride=stride,
+                            padding=((kernel_size-1)*d)//2,
+                            padding_mode=padding_mode,
+                            groups=groups,
+                            dilation=d
+                        )
+            except ValueError as e:
+                raise ValueError(f"Conv1 failed for in_channels={in_channels}, out_channels={out_channels},kernel_size={kernel_size},stride={stride},padding={((kernel_size-1)*d)//2},padding_mode={padding_mode},groups={groups},dilation={d} \n with error: {e}")
+            else:
+                self.convs.append(cnn)
 
     def forward(self,x):
         return torch.stack([conv(x).unsqueeze(2) for conv in self.convs],dim=2).view(x.shape[0],-1,x.shape[-1])
